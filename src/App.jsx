aa845b34458d6d7da4e76b0ecbcaead69e86938a
SionJi <jww019054@gmail.com>
2025-05-01 03:29:42 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Header from './components/Header'
 import EntryList from './components/EntryList'
 import NewEntryForm from './components/NewEntryForm'
